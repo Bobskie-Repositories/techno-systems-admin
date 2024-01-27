c11a89cc84115ba648cad7f959f420ec64a7d381
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2024-01-27 20:31:06 +0800

    update routes for admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,30 +11,13 @@ import Login from './screens/login';
 import Logout from './screens/logout';
 import ForgotPassword from './screens/forgot_password';
 import Signup from './screens/signup';
-import Classes from './screens/class_management/classes';
-import ViewClass from './screens/class_management/view_class';
-import ViewClassMembers from './screens/class_management/view_class_members';
-import Teams from './screens/team_management/teams';
-import PeerEval from './screens/peer_evaluation/view_peer_eval';
-import StudentPeerEval from './screens/peer_evaluation/view_student_peer_eval';
-import ClassroomLayout from './components/Layouts/ClassroomLayouts';
-import ActivityManagement from './screens/activity_management/activities';
 
-import SpringBoardProjects from './screens/springboard/projects';
-import SpringBoardAllProjects from './screens/springboard/all_projects';
-import SpringBoardTeacherAllProjects from './screens/springboard/teacher_view_all_projects';
-import ProjectView from './screens/springboard/project_view';
-import Rules from './screens/springboard/project_board_view/Rules/Rules';
-import AddBoard from './screens/springboard/project_board_view/AddBoard/AddBoard';
-import Result from './screens/springboard/project_board_view/Result/Result';
+import SpringBoardAdminAllProjects from './screens/springboard/admin_view_all_projects';
+import SpringBoardAdminTemplates from './screens/springboard/admin_templates';
+import CreateTemplate from './screens/springboard/admin_create_template/CreateTemplate';
+import EditTemplate from './screens/springboard/admin_edit_template/EditTemplate';
 import ViewBoard from './screens/springboard/project_board_view/ViewBoard/ViewBoard';
-import EditBoard from './screens/springboard/project_board_view/EditBoard/EditBoard';
-import EditBoardResult from './screens/springboard/project_board_view/EditBoard/EditBoardResult';
-import ClassTeamProjects from './screens/springboard/class_team_projects/ClassTeamProjects';
-import TeamProject from './screens/springboard/team_project_view/TeamProject';
 import SearchProject from './screens/springboard/search_project/SearchProject';
-
-import TeknoPlat from './screens/teknoplat';
 
 // Style Imports
 import './App.css';
@@ -70,140 +53,17 @@ function App() {
         <Route path="/forgotpassword" element={<ForgotPassword />} />
         <Route path="/logout" element={<Logout />} />
 
-        {/* Classroom routes */}
-        <Route path="classes">
+        <Route path="admin/allprojects">
           <Route
             index
             element={
               <PrivateRoute>
-                <Classes />
-              </PrivateRoute>
-            }
-          />
-
-          <Route path=":id" element={<ClassroomLayout />}>
-            <Route
-              index
-              element={
-                <PrivateRoute>
-                  <ViewClass />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="members"
-              element={
-                <PrivateRoute>
-                  <ViewClassMembers />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="teams"
-              element={
-                <PrivateRoute>
-                  <Teams />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="evals"
-              element={
-                <PrivateRoute>
-                  <StudentPeerEval />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="activities"
-              element={
-                <PrivateRoute>
-                  <ActivityManagement />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="projects"
-              element={
-                <PrivateRoute>
-                  <SpringBoardProjects />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="project/:projId"
-              element={
-                <PrivateRoute>
-                  <ProjectView />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="allteamprojects"
-              element={
-                <PrivateRoute>
-                  <ClassTeamProjects />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="team/:teamid"
-              element={
-                <PrivateRoute>
-                  <TeamProject />
-                </PrivateRoute>
-              }
-            />
-            <Route path="allprojects">
-              <Route
-                index
-                element={
-                  <PrivateRoute>
-                    <SpringBoardAllProjects />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="search-project/:projId"
-                element={
-                  <PrivateRoute>
-                    <SearchProject />
-                  </PrivateRoute>
-                }
-              />
-            </Route>
-
-            <Route
-              path="teknoplat"
-              element={
-                <PrivateRoute>
-                  <TeknoPlat />
-                </PrivateRoute>
-              }
-            />
-          </Route>
-        </Route>
-
-        <Route
-          path="/peer-eval"
-          element={
-            <PrivateRoute>
-              <PeerEval />
-            </PrivateRoute>
-          }
-        />
-
-        <Route path="/allprojects">
-          <Route
-            index
-            element={
-              <PrivateRoute>
-                <SpringBoardTeacherAllProjects />
+                <SpringBoardAdminAllProjects />
               </PrivateRoute>
             }
           />
           <Route
-            path="search-project/:projId"
+            path="team/:teamId/search-project/:projId"
             element={
               <PrivateRoute>
                 <SearchProject />
@@ -212,28 +72,28 @@ function App() {
           />
         </Route>
 
-        <Route path="/project/:id/create-board">
+        <Route path="admin/templates">
           <Route
-            path=":templateid/rules"
+            index
             element={
               <PrivateRoute>
-                <Rules />
+                <SpringBoardAdminTemplates />
               </PrivateRoute>
             }
           />
           <Route
-            path=":templateid/template"
+            path="add-template"
             element={
               <PrivateRoute>
-                <AddBoard />
+                <CreateTemplate />
               </PrivateRoute>
             }
           />
           <Route
-            path=":boardid/result"
+            path="template/:id"
             element={
               <PrivateRoute>
-                <Result />
+                <EditTemplate />
               </PrivateRoute>
             }
           />
@@ -245,22 +105,6 @@ function App() {
             element={
               <PrivateRoute>
                 <ViewBoard />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="edit"
-            element={
-              <PrivateRoute>
-                <EditBoard />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="edit/result"
-            element={
-              <PrivateRoute>
-                <EditBoardResult />
               </PrivateRoute>
             }
           />
